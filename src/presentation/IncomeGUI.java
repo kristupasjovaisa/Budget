dab61fade9dc68b46cb485dd01675752c9dcf6c6
kristupas <kristupas.jovaisa03@gmail.com>
2021-10-24 19:17:00 +0300

    Finished add expense gui implementation

--- src/presentation/IncomeGUI.java
+++ src/presentation/IncomeGUI.java
@@ -7,7 +7,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 import java.util.List;
 
 public class IncomeGUI implements ActionListener {
